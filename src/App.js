8e46b5e7ff2a0fb0720ad5f89f2c708b149b74b7
Faith <faithkim1158@gmail.com>
2020-06-21 10:18:02 -0400

    adding react router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import { AwesomeButton } from "react-awesome-button";
+import "react-awesome-button/dist/styles.css";
+//import AwesomeButtonStyles from "react-awesome-button/src/styles/styles.scss";
+
+function Home() {
+    return(
+        <div>
+            <h1 style={{color:"pink"}}>Hello World!</h1>
+//            <AwesomeButton type="secondary" href="/anotherpage">Click</AwesomeButton>
+        </div>
+    );
+}
+
+function AnotherPage() {
+    return (
+        <div>
+            <h1>Another page</h1>
+        </div>
+    )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Router>
+                <Route path="/" component={Home} exact />
+                <Route path="/anotherpage" component={AnotherPage} />
+            </Router>
+        </div>
+    );
 }
 
 export default App;
