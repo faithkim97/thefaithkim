8bcf431a3911059491de2490f33291ad98a63fb4
Faith <faithkim1158@gmail.com>
2020-06-21 15:11:18 -0400

    centering buttons

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,17 +1,21 @@
 import React, {Component} from 'react';
 import { AwesomeButton } from "react-awesome-button";
 import "react-awesome-button/dist/styles.css";
+import "./HomePageStyle.css";
 
 class HomePage extends Component {
     constructor(props) {
         super(props);
     }
     render() {
+        const height = (window.screen.height)/2;
         return(
-            <div>
-                <AwesomeButton type="secondary" href="/about">About</AwesomeButton>
-                <AwesomeButton type="secondary" href="/resume">Resume</AwesomeButton>
-                <AwesomeButton type="secondary" href="/projects">Projects</AwesomeButton>
+           <div style={{textAlign:"center"}}>
+            <div style={{position:"relative", bottom:-height, display:"inline-block"}}>
+                <AwesomeButton type="secondary" href="/about" style={{margin:"5px"}}>About</AwesomeButton>
+                <AwesomeButton type="secondary" href="/resume" style={{margin:"5px"}}>Resume</AwesomeButton>
+                <AwesomeButton type="secondary" href="/projects" style={{margin:"5px"}} >Projects</AwesomeButton>
+            </div>
             </div>
         );
 
