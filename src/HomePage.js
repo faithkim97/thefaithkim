b53844f5d6b223a8105cc6cff07e74af9fc0b3e3
Faith <faithkim1158@gmail.com>
2020-06-22 00:25:19 -0400

    resizing

--- src/HomePage.js
+++ src/HomePage.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { AwesomeButton } from "react-awesome-button";
 import "react-awesome-button/dist/styles.css";
 import TypeWriter from './TypeWriter';
+import Image from 'react-bootstrap/Image';
 
 class HomePage extends Component {
     constructor(props) {
@@ -10,15 +11,20 @@ class HomePage extends Component {
     render() {
         const height = (window.screen.height)/2;
         return(
-           <div>
-           <div style={{textAlign:"center"}}>
-                <TypeWriter text="Faith Kim" bottom={-height+30} />
-            <div style={{position:"relative", bottom:-height}}>
+           <div style={{position:"relative", bottom:-50}}>
+               <center><Image src='https://www.pikpng.com/pngl/b/275-2758906_hipster-transparent-aesthetic-easy-grunge-aesthetic-drawings-clipart.png'
+                                       style={{display:"inline-block",   width:"30%", height:"30%"}}
+                                       />
+               </center>
+
+            <center>
+            <TypeWriter text="Faith Kim" />
+            <div>
                 <AwesomeButton type="secondary" href="/about" style={{margin:"5px"}}>About</AwesomeButton>
                 <AwesomeButton type="secondary" href="/resume" style={{margin:"5px"}}>Resume</AwesomeButton>
                 <AwesomeButton type="secondary" href="/projects" style={{margin:"5px"}} >Projects</AwesomeButton>
             </div>
-            </div>
+            </center>
            </div>
         );
 
