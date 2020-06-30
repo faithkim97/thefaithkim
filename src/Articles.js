28ecfcaac88844547bfeb9abd2d79494a7b1ea47
Faith <faithkim1158@gmail.com>
2020-06-30 14:05:43 -0400

    fixed the size problem of featured articles

--- src/Articles.js
+++ src/Articles.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ProfilePic from './profilepic.jpg';
 import './SocialMediaIcon.css';
-import {Container, Row, Col} from 'react-bootstrap';
+import {Container, Row, Col, Card} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SmithCollegeArticle from './smithcollegearticle.png';
 import ReigningIt from './reigningit.png';
@@ -11,23 +11,29 @@ import GamingLabArticle from './gaminglab.png';
 function Articles(props) {
     return (
         <div className="fadeIn" style={{ bottom: "-200px", display:"inline-block", height:"auto", width:"100%", paddingBottom:"100px", paddingTop:"10px", position:"relative", overflow:"hidden"}}>
-            <div style={{display:"block", marginLeft:"-3%"}} >
+            <div style={{display:"block"}} >
                 <Container>
                     <Row>
-                        <Col md ="auto">
+                        <Col md ={4}>
+                        <Card>
                              <a href="https://www.smith.edu/profile/faith-kim">
-                                 <img id="featured" src={SmithCollegeArticle} />
+                                    <Card.Img id="featured" src={SmithCollegeArticle} />
                              </a>
+                         </Card>
                         </Col>
-                        <Col md="auto">
+                        <Col md={4}>
+                           <Card>
                             <a href="https://www.smith.edu/about-smith/news/digital-storytelling?fbclid=IwAR2HaFyUzPJ5MuxiUMksIuOUCU5rfB6k3zL8ii2PTWrxqz88mlI5MQM89Is">
-                                 <img id="featured" src={GamingLabArticle} />
+                                 <Card.Img id = "featured" src={GamingLabArticle} />
                             </a>
+                            </Card>
                          </Col>
-                        <Col sx = "auto">
+                        <Col sx = {4}>
+                            <Card>
                             <a href="https://medium.com/@ReigningIt/women-who-reign-faith-kim-1360a26c5c3b">
-                        <img id="featured" src={ReigningIt} />
+                               <Card.Img id="featured" src={ReigningIt} />
                           </a>
+                          </Card>
                         </Col>
                     </Row>
                 </Container>
